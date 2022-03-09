4f13eae440bdacf51851f3fc28ff6b9b494b1e59
aman802134 <pandeyamankumar8@gmail.com>
2022-03-09 16:15:26 +0530

    This is first commit of this folder

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// const toggler = document.querySelector(".toggler");
+// const navigationLinks = document.querySelector(".navigation-links")
+// toggler.addEventListener("click",()=>{
+//    if(navigationLinks.style.display === "none"){
+//        navigationLinks.style.display = "block";
+//    }else
+//    {
+//        navigationLinks.style.display = "none";
+//    }
+// })
